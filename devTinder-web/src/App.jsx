dd7307cb0f0ae979bb2cbda3d07985a51c907be3
amit-web <mehtaamit9334@gmail.com>
2024-10-03 16:35:43 +0530

    Routing and basic str of the reactjs

--- devTinder-web/src/App.jsx
+++ devTinder-web/src/App.jsx
@@ -1,11 +1,29 @@
-import { useState } from 'react'
+import { BrowserRouter,Routes,Route } from "react-router-dom"
+import Navbar from "./Navbar"
+import Body from "./Body"
+import Login from "./Login"
+import Profile from "./Profile"
 
 
 function App() {
 
   return (
     <>
-       <h1>Hello world</h1>
+    <BrowserRouter basename="/">
+
+    <Routes >
+        <Route path="/" element={<Body/>}>
+        
+               <Route path="/login" element={<Login/>}/>
+               <Route path="/profile" element={<Profile/>}/>
+        
+        </Route>
+        <Route path="/login" element={<div>Login page</div>}/>
+        <Route path="/test" element={<div>Test page</div>}/>
+    </Routes>
+    
+    </BrowserRouter>
+     {/* <Navbar/> */}
     </>
   )
 }
